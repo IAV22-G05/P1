1375b69f485105899ae4c170d72447d36f1e1ab1
Sergio Molinero <sergimol@ucm.es>
2022-02-16 20:25:39 +0100

    Bandada de ratas

---
+++ Assets/Scripts/Separacion.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCM.IAV.Movimiento
+{
+    public class Separacion : ComportamientoAgente
+    {
+        [SerializeField]
+        List<GameObject> objetivos;
+        [SerializeField]
+        float distancia;
+        [SerializeField]
+        float coeficiente;
+        [SerializeField]
+        float aceleracionMax;
+
+        public override Direccion GetDireccion()
+        {
+            Direccion total = new Direccion();
+            Direccion direccion = new Direccion();
+            float fuerza;
+            for(int i = 0; i < objetivos.Count; ++i)
+            {
+                direccion.lineal = objetivos[i].transform.position - transform.position;
+
+                if(direccion.lineal.magnitude < distancia)
+                {
+                    fuerza = Mathf.Min(coeficiente / (Mathf.Pow(distancia, 2)), aceleracionMax);
+                    direccion.lineal.Normalize();
+                    total.lineal += fuerza * direccion.lineal;
+                }
+            }
+            total.lineal.y = 0;
+            return total;
+        }
+    }
+}
