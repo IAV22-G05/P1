ab444e70006921bae8e29e93ef273e3a3cc87b22
Sergio Molinero <sergimol@ucm.es>
2022-02-17 00:37:06 +0100

    Ejecutable

--- Assets/Scripts/DebugInput.cs
+++ Assets/Scripts/DebugInput.cs
@@ -34,5 +34,9 @@ public class DebugInput : MonoBehaviour
                 i--;
             }
         }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
