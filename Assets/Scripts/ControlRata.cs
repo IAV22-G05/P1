d9d13325f661838a845dcd045083b219131b2902
Andrés Carnicero <acarni01@ucm.es>
2022-02-17 00:01:20 +0100

    Ajuste ratas

--- Assets/Scripts/ControlRata.cs
+++ Assets/Scripts/ControlRata.cs
@@ -9,11 +9,13 @@ public class ControlRata : MonoBehaviour
 
     Merodear merodear;
     Seguir seguir;
+    LlegadaDinamica llegada;
     // Start is called before the first frame update
     void Start()
     {
         merodear = GetComponent<Merodear>();
         seguir = GetComponent<Seguir>();
+        llegada = GetComponent<LlegadaDinamica>();
     }
 
     // Update is called once per frame
@@ -22,11 +24,13 @@ public class ControlRata : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             merodear.enabled = false;
+            llegada.enabled = true;
             seguir.enabled = true;
         }
         else if(Input.GetKeyUp(KeyCode.Space))
         {
             merodear.enabled = true;
+            llegada.enabled = false;
             seguir.enabled = false;
         }
     }
